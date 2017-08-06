ab2f054b4deacb5ccb7e449912f5b36511c270ce
qduy <qduy88@gmail.com>
2017-08-06 17:45:22 +0700

    update

---
+++ .circleci/build-gh.sh
@@ -0,0 +1,13 @@
+echo "Git Status"
+echo "==================="
+git status
+echo "==================="
+
+echo "Current directory is "
+pwd
+ls -l -a
+
+echo "\n\n"
+echo "Build is starting..."
+
+pelican
